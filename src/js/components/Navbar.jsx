66e0fe6580ccf8df43bc5137007350a904a5102a
Elizabeth Reynolds <e.d.reynolds@icloud.com>
2026-02-24 14:23:23 -0600

    created a basic navbar

---
+++ src/js/components/Navbar.jsx
@@ -0,0 +1,49 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Elizabeth Reynolds
+        </a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a
+                class="nav-link link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+                href="#"
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+                href="#"
+              >
+                Contact
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+                href="#"
+              >
+                My Work
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+                href="#"
+              >
+                Coming Soon
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
