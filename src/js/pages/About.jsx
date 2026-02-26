943fb774b22879a0377dd0fb27239fe234b2aa00
Elizabeth Reynolds <e.d.reynolds@icloud.com>
2026-02-26 07:34:43 -0600

    created a splitscreen background

--- src/js/pages/About.jsx
+++ src/js/pages/About.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Me</h1>
+    <div className="container-fluid p-0">
+      <div className="row g-0 vh-100">
+        <div
+          className="col-md-8  text-white d-flex align-items-center justify-content-center"
+          style={{ backgroundColor: "blueviolet" }}
+        >
+          <h1>Left Side</h1>
+        </div>
+        <div className="col-md-4 bg-dark text-white d-flex align-items-center justify-content-center">
+          <h1>Right Side</h1>
+        </div>
+      </div>
     </div>
   );
 };
